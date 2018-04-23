90ff2a94906a0e695097498295f2c564796d2835
Lucas Liu <lucas.liu@duke.edu>
2018-04-23 17:29:55 -0400

    new folder with extended API functionality

---
+++ src/API_Test/README.md
@@ -0,0 +1,38 @@
+Exploration
+Lucas Liu
+ltl9
+Start: 4/4/18
+End: 4/13/18
+Hours: 21
+Consulted With: N/A
+Resources: 
+New
+https://developers.google.com/api-client-library/javascript/features/discovery
+https://developers.google.com/api-client-library/javascript/reference/referencedocs#gapiclientinitargs
+
+
+Portions from in class mashup project, notably the use of vue2google for maps
+
+Promises - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then
+
+Google OAuth 2.0 - https://developers.google.com/identity/protocols/OAuth2UserAgent
+
+Google Calendar API:
+https://developers.google.com/calendar/quickstart/js
+https://developers.google.com/apis-explorer/?hl=en_US#p/calendar/v3/
+
+Assets: N/A
+
+Work:
+
+
+This is an exploration into Google OAuth 2.0 and APIs. Visually, the final product is not especially flashy, however a lot of behind the scenes work was put into the exploration. Hopefully, this ground level work becomes useful in the final project.
+
+Login to your Google account and give the proper permissions. Allow the site to find your location. It will be displayed with a blue circle.
+
+Press [Get Events]: The site will display your 10 upcoming events from Google Calendar. 
+If you add an event, a marker will be added in the corresponding location.
+
+Add an event to your calendar. Fill out all fields, and select a location using the autocomplete bar above the map. the map will switch to the location that you have entered. Click [Add Event] If all fields are complete, the event will be added to your Google Calendar.
+
+ Sign out as needed.
---
+++ src/testAPI.vue
@@ -0,0 +1,84 @@
+<template>
+    <div>
+        <head>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.6/vue.js"></script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.16.4/lodash.js"></script> 
+    <script src="vue-google-maps.js"></script> 
+            <style>
+        #map {
+            height: 60%;
+        }
+        html, body {
+            height: 80%;
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+    </head>
+    
+        <h1>LOCATOR</h1>
+            <gmap-autocomplete
+                           @place_changed="setPlace">
+        </gmap-autocomplete>
+
+        <button @click="usePlace">Add</button>
+        <br>
+        <button @click="addEvent">Add Event</button>
+        <gmap-map
+                  :center="center"
+                  :zoom="zoom"
+                  @click="addMarker"
+                  style="height: 480px;"
+                  >
+            <gmap-marker
+                         v-for="(m, index) in markers"
+                         :key="index"
+                         :position="m.position"
+                         :clickable="true"
+                         :draggable="true"
+                         @click="center = m.position"
+                         ></gmap-marker>
+            <gmap-marker
+                         v-if="this.place"
+                         label="Event Here"
+                         :draggable="false"
+                         :position="{
+                                    lat: this.place.geometry.location.lat(),
+                                    lng: this.place.geometry.location.lng(),
+                                    }"
+                         ></gmap-marker>
+            
+            <gmap-circle
+                         :center="pos"
+                         :radius="10"
+                         :options="{strokeColor: '#011AC7',
+                                   strokeOpacity: 0.8,
+                                   strokeWeight: 1,
+                                   fillColor: '#011AC7',
+                                   fillOpacity: 0.3}"
+                         ></gmap-circle>
+
+        </gmap-map>
+    
+    </div>
+</template>
+
+<script>
+   
+    
+    export default {
+        name:'Locator',
+        data () {
+            return {
+            }
+        },
+        props: [
+            
+        ],
+        
+    }
+</script>
+
+<style>
+    
+</style>
